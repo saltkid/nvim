659300e84da8f66dfcb8322cc98d00fedbd8dd38
saltkid <ojisaltkid@gmail.com>
2024-10-07 23:06:56 +0800

    feat(plugins): custom plugins
    remove tokyonight and todo-comments (for now)

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- I decided that each plugin with configuration should be in their own file
+  -- put plugins here that do not need configuration
+}
