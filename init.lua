d9b713a0dbdfa972b535fbcc26d97bacc0377e62
saltkid <ojisaltkid@gmail.com>
2024-11-18 23:50:03 +0800

    feat(set): set options for plugins post-setup by kickstart + set conform options
    hence, the moving of require customs to the bottom of the file in
    `init.lua`

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
-require 'custom.remaps.init'
-require 'custom.set.init'
-
 --[[
 
 =====================================================================
@@ -957,3 +954,8 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- OWN STARTS HERE
+require 'custom.remaps'
+require 'custom.set'
+-- OWN ENDS HERE
