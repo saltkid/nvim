e6d223c29050e28a8bd1e61ee9fac0c273253ba7
saltkid <ojisaltkid@gmail.com>
2024-12-01 21:45:48 +0800

    feat(remaps): show diagnostic errors under cursor in floating window

--- lua/custom/remaps/init.lua
+++ lua/custom/remaps/init.lua
@@ -8,7 +8,7 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
 -- find and replace all (in file) (thx prime)
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[S]earch and Replace' })
+vim.keymap.set('n', '<leader>rw', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[R]eplace [W]ord' })
 
 -- they're the same thing lul
 vim.keymap.set('i', '<C-c>', '<Esc>')
@@ -16,9 +16,6 @@ vim.keymap.set('i', '<C-c>', '<Esc>')
 -- never press this I guess.
 -- After some reading, this is apparently 'ex' mode. not touching this lol
 vim.keymap.set('n', 'Q', '<nop>')
-
--- select all
-vim.keymap.set('n', '<C-a>', 'ggVG')
 
 -- move single lines
 vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
@@ -31,20 +28,14 @@ vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selected lines d
 vim.keymap.set('v', '>', '>gv', { desc = 'Indent to the right' })
 vim.keymap.set('v', '<', '<gv', { desc = 'Indent to the left' })
 
--- delete without yanking
-vim.keymap.set('n', '<leader>d', '"_d', { desc = 'Delete without yanking' })
-vim.keymap.set('v', '<leader>d', '"_d', { desc = 'Delete without yanking' })
--- delete and go into insert mode without yanking
-vim.keymap.set('n', '<leader>c', '"_c', { desc = 'Delete and go into insert mode without yanking' })
-vim.keymap.set('v', '<leader>c', '"_c', { desc = 'Delete and go into insert mode without yanking' })
--- paste over selection without yanking
-vim.keymap.set('v', '<leader>p', '"_dP', { desc = 'Paste without yanking' })
-
 -- buffers
 vim.keymap.set('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Previous buffer' })
 vim.keymap.set('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
 vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<cr>', { desc = 'Delete buffer' })
 vim.keymap.set('n', '<leader>`', '<cmd>e #<cr>', { desc = 'Switch to recent buffer' })
+
+-- Diagnostic
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rrors under cursor in float' })
 
 -- lazygit
 vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<cr>', { desc = 'Open lazygit' })
